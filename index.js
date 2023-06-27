868d1e891db5b885a4a1ef10b487c0e3992c2ae5
RahulM3 <rahulcode97@gmail.com>
2023-06-28 00:42:17 +0530

    fixed slide show errors/bugs

---
+++ index.js
@@ -0,0 +1,21 @@
+if(document.readyState == 'complete'){
+	document.getElementById('radio1').checked = true;
+}
+setInterval(function(){
+	var s1 = document.getElementById('radio1');
+	var s2 = document.getElementById('radio2');
+	var s3 = document.getElementById('radio3');
+	var s4 = document.getElementById('radio4');
+	if(s1.checked){
+		s2.checked = true;
+	}
+	else if(s2.checked){
+		s3.checked = true;
+	}
+	else if(s3.checked){
+		s4.checked = true;
+	}
+	else{
+		s1.checked = true;
+	}
+}, 5000);
