67d25e190b82fb60748fdf34d1a4af4529e54adb
RahulM3 <rahulcode@gmail.com>
2023-07-11 15:10:00 +0530

    Tyre selection

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ window.addEventListener("load", function(){
 	document.getElementById("loadingscreen").remove();
 })
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 if(document.readyState == 'complete'){
 	document.getElementById('radio1').checked = true;
@@ -25,23 +28,24 @@ setInterval(function(){
 	}
 }, 5000);
 
-other_ele = {"bike":-10, "car":-20, "tractor":-30, "truck":-40};
+margin_left = "-100%"
+resert_mragin = {"bike":"-150%", "car":"-150%", "truck":"-150%", "tractor":"-150%"}
 key = ["bike", "car", "tractor", "truck"]
 others_hidden = false;
-function expand(ele){
+async function expand(ele){
 	if(others_hidden == false){
 		for(var i=0; i<key.length; i++){
 			if(key[i] != ele.id){
 				document.getElementById(key[i]).setAttribute("hidden", "hidden");
 			}
 			else{
-				document.getElementById("tyreslider").style.marginLeft = other_ele[ele.id].toString()+"%";
+				document.getElementById(ele.id).style.marginLeft = resert_mragin[ele.id];
 			}
 		}
 		others_hidden = true;
 	}
 	else if(others_hidden){
-		document.getElementById("tyreslider").style.marginLeft = 0;
+		document.getElementById(ele.id).style.marginLeft = 0;
 		for(var i=0; i<key.length; i++){
 			document.getElementById(key[i]).removeAttribute("hidden");
 		}
