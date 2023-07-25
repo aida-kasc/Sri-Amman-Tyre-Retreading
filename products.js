3a6ab5f317b0ee879b89c1947cbfcea77c201c38
RahulM-3 <rahulcode97@gmail.com>
2023-07-25 09:33:12 +0530

    Product page

--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function(){
 	document.getElementById("loadingscreen").remove();
+	load();
 })
 
 async function getdata(url){
@@ -9,13 +10,26 @@ async function getdata(url){
 }
  
 var tyres;
+var total_pages;
+var total_entity;
+var cur_page = 1;
 (async () => {
-	tyres = await getdata("populartyres.json");
+	tyres = await getdata("allproducts.json");
+	tyres = tyres["tyres"]
+	total_entity = Object.keys(tyres).length;
+	total_pages = Math.ceil(total_entity/25);
 })();
-console.log(tyres)
 
-function load(keyword, filter){
-    console.log(keyword, filter);
+async function load(keyword=null, filter="All"){
+	console.log(cur_page)
+    for(var i=25*(cur_page-1); i<25*(cur_page); i++){
+    	for(var j=1; j<=5; j++, i++){
+    		if(tyres[i] != null){
+    			console.log(document.getElementsByClassName("col"+toString(j)).class);
+    		}
+    	}
+    }
+
 }
 var filterval = "All";
 function filter(ele){
@@ -26,4 +40,22 @@ function search(){
     document.getElementById("searchicon").src = "images/search.gif";
     keyword = document.getElementById("search").value;
     load(keyword, filterval)
+}
+
+function prev_page(ele){
+	cur_page--;
+	if(cur_page == 1){
+		ele.setAttribute("disabled", "disabled");
+	}
+	document.getElementById("next_page").removeAttribute("disabled");
+	load();
+}
+
+function next_page(ele){
+	cur_page++;
+	if(cur_page == total_pages){
+		ele.setAttribute("disabled", "disabled");
+	}
+	document.getElementById("prev_page").removeAttribute("disabled");
+	load();
 }
