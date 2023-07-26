df0a2123c3c742863e16ea009b3d9ebca406a307
RahulM-3 <rahulcode97@gmail.com>
2023-07-26 22:27:28 +0530

    product display update

--- products.js
+++ products.js
@@ -13,24 +13,35 @@ var tyres;
 var total_pages;
 var total_entity;
 var cur_page = 1;
+var cur_entry = 0;
+
 (async () => {
-	tyres = await getdata("allproducts.json");
-	tyres = tyres["tyres"]
+	tyres = await getdata("https://sri-amman-tyre-retreading-default-rtdb.firebaseio.com/all_products.json");
 	total_entity = Object.keys(tyres).length;
 	total_pages = Math.ceil(total_entity/25);
 })();
 
 async function load(keyword=null, filter="All"){
-	console.log(cur_page)
-    for(var i=25*(cur_page-1); i<25*(cur_page); i++){
-    	for(var j=1; j<=5; j++, i++){
-    		if(tyres[i] != null){
-    			console.log(document.getElementsByClassName("col"+toString(j)).class);
-    		}
+	total_cols = [];
+    for(var j=1; j<=5; j++){
+    	cols = document.getElementById("row"+(j).toString()).childNodes;
+    	for(var k=1; k<=9; k+=2){
+    		total_cols.push(cols[k]);
     	}
     }
 
+    for(var i=0; i<25; i++){
+    	itemno = cur_entry+i
+    	if(tyres[itemno] !=  null){
+    		total_cols[i].innerHTML = "<div class='imgwrapper'><img src='"+tyres[itemno][0]+"'></div>";
+    		total_cols[i].innerHTML += "<p>"+tyres[itemno][1]+"</p>";
+    	}
+    	else{
+    		total_cols[i].style.display = "none";
+    	}
+    }
 }
+
 var filterval = "All";
 function filter(ele){
     filterval = ele.value
@@ -43,6 +54,7 @@ function search(){
 }
 
 function prev_page(ele){
+	cur_entry -= 25;
 	cur_page--;
 	if(cur_page == 1){
 		ele.setAttribute("disabled", "disabled");
@@ -52,6 +64,7 @@ function prev_page(ele){
 }
 
 function next_page(ele){
+	cur_entry += 25;
 	cur_page++;
 	if(cur_page == total_pages){
 		ele.setAttribute("disabled", "disabled");
